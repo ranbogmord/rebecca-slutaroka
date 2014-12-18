eda55578435ed03c980ad71634df64fd9eb6befe
John Eriksson <john@provideit.se>
2014-12-18 22:54:57 +0100

    Added logout

--- wp-content/themes/slutaroka/footer.php
+++ wp-content/themes/slutaroka/footer.php
@@ -1,3 +1,6 @@
+    <?php if (is_user_logged_in()) : ?>
+        <a href="<?php echo wp_logout_url(site_url('/login')); ?>">Logga ut</a>
+    <?php endif; ?>
     </div> <!-- Avslutar #main i header.php -->
     <!-- wp_footer för att ladda in javascript genom wp -->
     <?php wp_footer(); ?>
